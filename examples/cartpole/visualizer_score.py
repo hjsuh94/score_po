ce9081281e8c7cbd4d30247c22df5f4dd1ccebb8
Hongkai Dai <daihongkai@gmail.com>
2023-05-02 23:00:35 -0700

    Tune ScoreEstimator log p(x, u, x') for Cartpole. Use a larger network.

--- examples/cartpole/visualizer_score.py
+++ examples/cartpole/visualizer_score.py
@@ -4,11 +4,12 @@ import os
 import hydra
 import torch
 
+from examples.cartpole.cartpole_plant import CartpolePlant
 from examples.cartpole.score_training import get_score_network, draw_score_result
 from score_po.score_matching import (
     ScoreEstimatorXu,
     ScoreEstimatorXux,
-    langevin_dynamics,
+    NoiseConditionedScoreEstimatorXux,
 )
 
 
@@ -16,7 +17,7 @@ from score_po.score_matching import (
 def main(cfg: DictConfig):
     OmegaConf.save(cfg, os.path.join(os.getcwd(), "config.yaml"))
     network = get_score_network(cfg.single_shooting)
-    score_estimator_cls = ScoreEstimatorXu if cfg.single_shooting else ScoreEstimatorXux
+    score_estimator_cls = ScoreEstimatorXu if cfg.single_shooting else NoiseConditionedScoreEstimatorXux
     score_estimator = score_estimator_cls(dim_x=4, dim_u=1, network=network).to(cfg.device)
     if cfg.single_shooting:
         load_ckpt = cfg.score_estimator_xu_load_ckpt
@@ -24,10 +25,13 @@ def main(cfg: DictConfig):
         load_ckpt = cfg.score_estimator_xux_load_ckpt
     score_estimator.load_state_dict(torch.load(load_ckpt))
 
-    for eps in (1E-2, 1E-3):
-        for steps in (1000, 10000):
+    plant = CartpolePlant(dt=cfg.plant_param.dt)
+
+    for eps in (1E-1, 1E-2,1E-3):
+        for steps in (100, 1000, 10000, 100000):
             draw_score_result(
                 score_estimator,
+                plant,
                 cfg.device,
                 epsilon=eps,
                 steps=steps,
