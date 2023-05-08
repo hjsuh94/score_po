ec09f9f7693dc183d4a2faa5b8b4f43641963f62
lujieyang <lujie@mit.edu>
2023-05-08 09:58:31 -0400

    Fix small error

--- score_po/trajectory_optimizer.py
+++ score_po/trajectory_optimizer.py
@@ -334,10 +334,13 @@ class TrajectoryOptimizerSS(TrajectoryOptimizer):
         assert isinstance(self.trj, SSTrajectory)
         assert isinstance(self.sf, ScoreEstimatorXu)
 
-    def initialize(self):
-        u_trj_init = torch.zeros(self.trj.u_trj.shape).to(self.params.device)
-        u_trj_init += torch.randn_like(u_trj_init) * 0.0
-        self.trj.u_trj = torch.nn.Parameter(u_trj_init)
+    def initialize(self, x_trj_guess, u_trj_guess):
+        if u_trj_guess is None:
+            u_trj_init = torch.zeros(self.trj.u_trj.shape).to(self.params.device)
+            u_trj_init += torch.randn_like(u_trj_init) * 0.1
+            self.trj.u_trj = torch.nn.Parameter(u_trj_init)
+        else:
+            self.trj.u_trj = torch.nn.Parameter(u_trj_guess)
 
     def rollout_trajectory(self):
         """
